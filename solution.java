424f9b03467973f813b56ea99c06ccbd77c1f97a
Kooreis <kooreis.official@gmail.com>
2024-05-28 05:33:23 -0500

    Created the main class and method. Initialized a Scanner object for user input and a LinkedList to store the characters of the input string. The program then prompts the user to enter a string, reads the input, and adds each character to the LinkedList.

--- solution.java
+++ solution.java
@@ -1,2 +1,13 @@
-import java.util.LinkedList;
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Character> linkedList = new LinkedList<>();
+
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+
+        for (char c : input.toCharArray()) {
+            linkedList.add(c);
+        }
+    }
+}
